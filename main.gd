00fa69bf98750e224e50b08ce8ade98a367d0b53
Dams4K <dams4k@users.noreply.github.com>
2026-06-08 19:06:58 +0200

    neoforge installation

---
+++ main.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var mc_launcher: MCProfileLauncher = $MCProfileLauncher
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+func _ready() -> void:
+	HTTPClientPool.task_queued.connect(
+		func():
+			progress_bar.max_value = max(1, HTTPClientPool.total_to_download)
+	)
+	HTTPClientPool.task_completed.connect(
+		func():
+			progress_bar.value = HTTPClientPool.total_downloaded
+	)
+	
+	mc_launcher.install()
