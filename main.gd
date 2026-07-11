ad71cbacd8828ef91a91c7d81da0e090691efb1e
Dams4K <82527785+Dams4K@users.noreply.github.com>
2026-07-11 11:06:45 +0200

    update modpack installation

--- main.gd
+++ main.gd
@@ -24,6 +24,7 @@ func _ready() -> void:
 		username_line_edit.text = config.get_value("settings", "name", "")
 		ram_slider.value = config.get_value("settings", "ram", 4)
 	
+	_on_ram_slider_value_changed(ram_slider.value)
 	modpack_versions = DownloadTask.new()
 	modpack_versions.url = MODPACKS_URL
 	modpack_versions.completed.connect(_on_versions_task_completed)
@@ -93,12 +94,57 @@ func _on_launch_button_pressed() -> void:
 	var files_url: String = modpack.files_url
 	var files_task := DownloadTask.new()
 	files_task.url = files_url
+	
 	var files: Dictionary = (await HTTPClientPool.download(files_task).wait()).json()
+	var old_files_data: Dictionary = {}
+	
+	var old_files_path = profile.game_directory.path_join("modpacks/%s" % modpack.id)
+	if not FileAccess.file_exists(old_files_path.get_base_dir()):
+		DirAccess.make_dir_recursive_absolute(old_files_path.get_base_dir())
+	
+	if FileAccess.file_exists(old_files_path):
+		var old_files_file = FileAccess.open(old_files_path, FileAccess.READ)
+		old_files_data = old_files_file.get_var(false)
+	
+	await update_modpack_installation(profile, old_files_data, files)
+	var files_data = FileAccess.open(old_files_path, FileAccess.WRITE)
+	files_data.store_var(files, false)
+	
+	mc_launcher._launch(profile, profile.authenticator)
+
+func update_modpack_installation(profile: MCProfile, previous_data: Dictionary, new_data: Dictionary):
 	var file_tasks: Array[DownloadTask] = []
 	
-	for file_rel_path: String in files:
+	var files_to_remove: Array[String] = []
+	
+	for file_rel_path: String in previous_data:
+		var file_info: Dictionary = new_data[file_rel_path]
+		var file_sha1 = file_info.sha1
+		if file_rel_path in new_data:
+			if new_data[file_rel_path].sha1 == file_sha1:
+				# Même version, pas besoin de s'en occuper, si ce n'est pas la même version, le fichier sera réécrit
+				new_data.erase(file_rel_path)
+		else:
+			files_to_remove.append(file_rel_path)
+	
+	if files_to_remove.is_empty():
+		Log.debug("Nothing to remove")
+	
+	for file_rel_path in files_to_remove:
 		var file_path: String = profile.game_directory.path_join(file_rel_path)
-		var file_info: Dictionary = files[file_rel_path]
+		Log.info("Remove file '%s'" % file_path)
+		if DirAccess.remove_absolute(file_path) != OK:
+			Log.error("Failed to remove file '%s'" % file_path)
+	
+	if new_data.is_empty():
+		Log.info("Nothing to update")
+	else:
+		Log.debug("Data to install: {}".format(new_data))
+	
+	for file_rel_path: String in new_data:
+		Log.debug(file_rel_path)
+		var file_path: String = profile.game_directory.path_join(file_rel_path)
+		var file_info: Dictionary = new_data[file_rel_path]
 		var file_url = file_info.url
 		var file_size = file_info.size
 		var file_sha1 = file_info.sha1
@@ -113,8 +159,7 @@ func _on_launch_button_pressed() -> void:
 		file_tasks.append(file_task)
 	
 	await DownloadTask.wait_all(file_tasks)
-	mc_launcher._launch(profile, profile.authenticator)
 
 
 func _on_ram_slider_value_changed(value: float) -> void:
-	ram_label.text = "Ram: %sGo" % value
+	ram_label.text = "Ram: %dGo" % value
